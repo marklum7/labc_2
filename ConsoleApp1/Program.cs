0db6be434db7e50782ebb14588411be167c58d07
vapil <susulya00@mail.ru>
2024-02-15 10:50:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Main {
+public Main() {
+        Console.WriteLine("Введите N:");
+        int N = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите x:");
+        int x = int.Parse(Console.ReadLine());
+        double result = 0.0;
+        if (x < 1)
+        {
+            for (int i = 0; i <= N; ++i)
+            {
+                result = result + Math.Pow((double)x, (double)(2 * x + 1)) / 2.0 * (double)i + 1.0;
+            }
+
+            Console.WriteLine(2.0 * result);
+        }
+        else
+        {
+            Console.WriteLine("ОШИБКА");
+        }
+
+    }
+
+}
